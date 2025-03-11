056152940c02976c5ab2284fc81df9beef8b0dd8
AlfaBotan <ilya.voloshik@gmail.com>
2025-03-11 15:03:32 +0300

    Initial Commit

---
+++ Rick and Morty Explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick and Morty Explorer
+//
+//  Created by Илья Волощик on 11.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
